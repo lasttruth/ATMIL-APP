097175045d6975b1cf23340d9685ee739327d247
Robin Doirin <robindoirin@gmail.com>
2019-12-04 10:50:39 -0500

    Added Top side and 3/4 views buttons

---
+++ MyFirstGame/Assets/Scripts/SwtichCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwtichCamera : MonoBehaviour
+{
+    public GameObject FrontCamera = null;
+    public GameObject SecondCamera = null;
+    public int counter = 0;
+    public void CamerSwitch()
+    {
+        //swtiching camera views based on what camera is placed into the editor
+        counter++;
+        if (counter % 2 == 1)
+        {
+            SecondCamera.SetActive(true);
+            FrontCamera.SetActive(false);
+
+        }
+        else
+        {
+            FrontCamera.SetActive(true);
+            SecondCamera.SetActive(false);
+        }
+    }
+}
